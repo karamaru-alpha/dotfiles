0d68deeb0764359e07bacc1ca4f2c15978bc4911
karamaru-alpha <mrnk3078@gmail.com>
2024-04-14 17:01:46 +0900

    update: install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-
 ln -sfnv "${SCRIPT_DIR}"/.zshrc ~
 ln -sfnv "${SCRIPT_DIR}"/.config/ ~
+
+# brew install mise watch tree fzf git sheldon kubectl minikube neovim raycast
